c88ad80d4c028e9b06185e55283d663fd53b66f7
lourduradjou <rajlourdu15@gmail.com>
2025-04-25 00:28:15 +0530

    Added toast with upload image cloudinary api

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,11 +5,13 @@ const PRODUCT_API_URL = 'http://localhost:8000/products'
 class Product {
     async getAllProducts() {
         const response = await axios.get(`${PRODUCT_API_URL}`)
+        console.log(response.data)
         return response.data
     }
 
     async getProductById(id) {
         const response = await axios.get(`${PRODUCT_API_URL}/${id}`)
+        console.log(response.data)
         return response.data
     }
 }
